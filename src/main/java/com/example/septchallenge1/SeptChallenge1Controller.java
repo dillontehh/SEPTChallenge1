d5574710b2a7853bbb8be31cc11f9ee0acf0e011
dillontehh <82991278+dillontehh@users.noreply.github.com>
2022-09-09 16:34:20 +1000

    final

--- src/main/java/com/example/septchallenge1/SeptChallenge1Controller.java
+++ src/main/java/com/example/septchallenge1/SeptChallenge1Controller.java
@@ -1,14 +1,52 @@
 package com.example.septchallenge1;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class SeptChallenge1Controller {
 
-    @RequestMapping
-    public String testController(){
-        return "Hello World";
+    @RequestMapping(path = "/item")
+    public Class itemController(){
+
+        @Autowired
+        private ItemDAO itemDao;
+
+        @GetMapping(path="/", produces = "application/json")
+        public Item getEmployees(@PathVariable String id)
+        {
+            Item returnItem = new Item();
+            boolean found = false;
+
+            for (int i = 0; i < itemDao.getItemsList().getItemList().size() && !found; ++i)
+                if (Objects.equals(itemDao.getItemsList().getItemList().get(i).getId(), id)) {
+                    returnItem = itemDao.getItemsList().getItemList().get(i);
+                    found = true;
+                }
+
+            return returnItem;
+        }
+
+        @PostMapping(path= "/", consumes = "application/json", produces = "application/json")
+        public Item addItem(@RequestBody Item newItem)
+        {
+            itemDao.addItem(newItem);
+            return itemDao.getItemsList().getItemList().get(Integer.parseInt(newItem.getId()));
+        }
+
+        @PutMapping(path="/{id}")
+        public Item updateItem(@PathVariable String id, @RequestBody Item item)
+        {
+            return itemDao.updateItem(id, item);
+        }
+
+        @DeleteMapping(path = "/{id}")
+        public Item deleteItem(@PathVariable String id)
+        {
+            return itemDao.deleteItem(id);
+        }
+    }
     }
 
 }
--- src/main/java/com/example/septchallenge1/SeptChallenge1Dao.java
+++ src/main/java/com/example/septchallenge1/SeptChallenge1Dao.java
@@ -1,4 +1,8 @@
 package com.example.septchallenge1;
 
 public class SeptChallenge1Dao {
+    private static Items list = new Items();
+
+    static
+    {}
 }
